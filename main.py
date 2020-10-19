7c72780b4cc2760053ca0a2743aa61f4ec3e00a7
Minh Hoang <nbmhoang.17it1@sict.udn.vn>
2020-10-19 10:54:13 +0700

    Bug fixes

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ import xml.etree.ElementTree as ET
 def update_xml_file(xml_file: str):
   tree = ET.parse(xml_file)
   root = tree.getroot()
-  if root.find('object') is not None:
-    for ele in root.find('object'):
-      if ele.tag in ('truncated', 'difficult'):
-        ele.text = '0'
+  for obj in root.findall('object'):
+      # print('Found a object')
+      for ele in obj:
+        if ele.tag in ('truncated', 'difficult'):
+          ele.text = '0'
   tree.write(xml_file)
 
 if __name__ == "__main__":
-  DATA = 'data'
+  DATA = 'rewrite'
   for folder in os.listdir(DATA):
     current_path = os.path.join(DATA, folder)
     if os.path.isdir(current_path):
